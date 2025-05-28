4575f0ad1ccd6d4da9a8b7e955b07f020415ec4d
Gabriel Gallan <gabriel31.gal@gmail.com>
2025-05-28 17:08:11 -0300

    fix: CORS active

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import fastity from 'fastify'
 import { userRoutes } from './routes/accountsRoutes'
+import cors from '@fastify/cors'
 
 export const app = fastity()
+
+app.register(cors)
 
 //Registrando as rotas do servidor
 app.register(userRoutes)
